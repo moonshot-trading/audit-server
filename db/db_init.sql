2ff8c098354cff2d4c7b0ae33db9034881194e60
Geoff Lorne <geo.lorne@gmail.com>
2018-01-24 16:02:46 -0800

    increase size of cyrptokey

--- db/db_init.sql
+++ db/db_init.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS audit_log (
   stockSymbol       VARCHAR(3),
   filename          VARCHAR(20),
   funds             INT,
-  cryptokey         VARCHAR(20),
+  cryptokey         VARCHAR(32),
   price             INT,
   quoteServerTime   BIGINT,
   action            VARCHAR(20),
