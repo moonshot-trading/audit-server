382f3fca2fe57233730fc7ff8e2282b94e52002e
Tanner Zinck <tzinck@uvic.ca>
2018-03-19 17:24:58 -0700

    load basic config from env vars when running in docker

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/jackc/pgx/stdlib"
 )
@@ -10,16 +11,15 @@ import (
 var (
 	config = auditConfig{func() string {
 		if runningInDocker() {
-			return "audit-db"
+			return os.Getenv("AS_DB_HOST")
 		} else {
 			return "localhost"
 		}
 	}()}
-	db = loadDB()
+	db       = loadDB()
+	SERVER   = os.Getenv("AS_SERVER_ENUM")
+	FILENAME = os.Getenv("FILENAME")
 )
-
-const SERVER = "1"
-const FILENAME = "10userWorkLoad"
 
 func loadDB() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.db, 5432, "moonshot", "hodl", "moonshot-audit")
